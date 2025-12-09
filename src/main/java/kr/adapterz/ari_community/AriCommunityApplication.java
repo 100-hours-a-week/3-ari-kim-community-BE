886a65a50a71abb1e6cd27cf6eae9e7733ac9d16
dvin-22 <dain5835@gmail.com>
2025-12-09 16:20:05 +0900

    fix: 작성시각 한국 기준으로 설정

--- src/main/java/kr/adapterz/ari_community/AriCommunityApplication.java
+++ src/main/java/kr/adapterz/ari_community/AriCommunityApplication.java
@@ -1,10 +1,18 @@
 package kr.adapterz.ari_community;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AriCommunityApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AriCommunityApplication.class, args);
